f068bb92a2ef224499d958adb7ee9d2432c6d156
Tom Maravilla <tlmaravilla@outlook.com>
2019-10-23 21:58:58 -0600

    working on subcategory create with view models

---
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,115 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models;
+using Spice.Models.ViewModels;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.SubCategory.Include(s => s.Category).ToListAsync());
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var model = new SubCategoryAndCategoryViewModel();
+            model.CategoryList = await _db.Category.ToListAsync();
+            model.SubCategory = new SubCategory();
+            model.SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(s => s.Name).Distinct().ToListAsync();
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var result = await _db.Category.FindAsync(id);
+            if (result == null)
+                return NotFound();
+
+            return View(result);
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create()
+        //{
+        //    var model = new SubCategoryAndCategoryViewModel();
+        //    model.CategoryList = await _db.Category.ToListAsync();
+        //    model.SubCategory = new SubCategory();
+        //    model.SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(s => s.Name).Distinct().ToListAsync();
+
+        //    return View(model);
+        //}
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var subCategory = await _db.SubCategory.FindAsync(id);
+            if (subCategory == null)
+                return NotFound();
+
+            return View(subCategory);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(category);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return View();
+            }
+            _db.Category.Remove(category);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
